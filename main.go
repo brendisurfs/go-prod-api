5a9785e9d5e2e19a86f8dc7c80e7f39860bda75b
Brendan Prednis <brendan.prednis@protonmail.com>
2021-08-20 14:28:03 -0700

    structuring application.

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like pointers
+// to database connections
+type App struct {
+}
+
+// extend App struct method
+func (app *App) Run() error {
+	fmt.Println("setting up our app.")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go Rest API")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting app.")
+	}
 }
