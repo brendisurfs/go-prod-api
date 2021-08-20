3ce74fb444c607b822d615f9e8c39dd4071704b5
Brendan Prednis <brendan.prednis@protonmail.com>
2021-08-20 14:36:38 -0700

    added health check endpoint.

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/brendisurfs/golang-prod-api/internal/transport/http"
+)
 
 // App - the struct which contains things like pointers
 // to database connections
@@ -10,7 +15,16 @@ type App struct {
 // extend App struct method
 func (app *App) Run() error {
 	fmt.Println("setting up our app.")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("failed to set up server")
+		return err
+	}
+
 	return nil
+
 }
 
 func main() {
